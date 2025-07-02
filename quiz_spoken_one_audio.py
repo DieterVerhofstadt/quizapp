d0054d7efff350f08c3e23c398f1a92385598ffa
DieterVerhofstadt <dieter.verhofstadt@gmail.com>
2025-07-02 23:43:00 +0200

    Update quiz_spoken_one_audio.py
    herhaal ipv stilte

--- quiz_spoken_one_audio.py
+++ quiz_spoken_one_audio.py
@@ -14,29 +14,27 @@ def create_tts_mp3(text):
     tts.write_to_fp(mp3_fp)
     return mp3_fp.getvalue()
 
-def create_silence(duration=5):
+def herhaal:
     # Simuleer stilte met herhaling van "uh"
-    text = " ".join(["Simulatie van stilte"] * duration)
+    text = "Ik herhaal:"
     return create_tts_mp3(text)
-
+    
 if st.button("Start quiz"):
     with open('vragen.csv', newline='', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
         vragenlijst = list(reader)
 
-    random.shuffle(vragenlijst)
+   random_10_vragen = random.sample(vragenlijst, 10)
 
     combined_mp3 = b""
 
-    for rij in vragenlijst:
+    for rij in random_10_vragen:
         vraag = rij['vraag']
         antwoord = rij['antwoord']
-
         vraag_audio = create_tts_mp3(vraag)
-        stilte = create_silence(duration=3)  # ongeveer 3 seconden “stilte”
         antwoord_audio = create_tts_mp3(antwoord)
 
-        combined_mp3 += vraag_audio + stilte + antwoord_audio
+        combined_mp3 += vraag_audio + herhaal + vraag_audio + antwoord_audio
 
     st.audio(io.BytesIO(combined_mp3), format="audio/mp3")
     st.download_button("Download mp3", data=combined_mp3, file_name="quiz.mp3", mime="audio/mpeg")
