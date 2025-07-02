03085cfeab41d5fd7e5d5523b5d3eaeba4d26871
DieterVerhofstadt <dieter.verhofstadt@gmail.com>
2025-07-02 19:09:28 +0200

    Update quiz_spoken.py
    versie met bugfix indent

--- quiz_spoken.py
+++ quiz_spoken.py
@@ -11,20 +11,18 @@ with open('vragen.csv', newline='', encoding='utf-8') as csvfile:
 
 random.shuffle(vragenlijst)  # schud de lijst door elkaar
 
-for rij in vragenlijst:
+for i, rij in enumerate(vragenlijst):
     vraag = rij['vraag']
     antwoord = rij['antwoord']
 
- tts_vraag = gTTS(text=vraag, lang='nl')
-    vraag_file = f"vraag_{st.session_state.index}.mp3"
+    tts_vraag = gTTS(text=vraag, lang='nl')
+    vraag_file = f"vraag_{i}.mp3"
     tts_vraag.save(vraag_file)
     st.audio(vraag_file, format='audio/mp3')
 
     time.sleep(5)  # wacht 5 seconden
 
- tts_antwoord = gTTS(text=antwoord, lang='nl')
-    antwoord_file = f"antwoord_{st.session_state.index}.mp3"
+    tts_antwoord = gTTS(text=antwoord, lang='nl')
+    antwoord_file = f"antwoord_{i}.mp3"
     tts_antwoord.save(antwoord_file)
     st.audio(antwoord_file, format='audio/mp3')
-
-    time.sleep(2)  # korte pauze voor volgende vraag
