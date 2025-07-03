710df166c0433eae69876cd68af8a526dead0efc
DieterVerhofstadt <dieter.verhofstadt@gmail.com>
2025-07-03 23:35:08 +0200

    Update quiz_spoken_one_audio.py
    kunstmatige pauze

--- quiz_spoken_one_audio.py
+++ quiz_spoken_one_audio.py
@@ -35,6 +35,6 @@ if st.button("Start quiz"):
         pauze_audio = pauze()
         antwoord_audio = create_tts_mp3(antwoord)
         volgende = create_tts_mp3("Volgende vraag")
-        combined_mp3 += vraag_audio + pauze_audio + antwoord_audio + volgende
+        combined_mp3 += vraag_audio + pauze_audio + pauze_audio + pauze_audio + pauze_audio + pauze_audio + antwoord_audio + volgende
 
     st.audio(io.BytesIO(combined_mp3), format="audio/mp3")
