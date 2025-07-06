04cab439123a0f0e10e41e19c568645e3dd39308
DieterVerhofstadt <dieter.verhofstadt@gmail.com>
2025-07-07 00:42:33 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,11 @@
+def create_tts_mp3(text):
+    mp3_fp = io.BytesIO()
+    tts = gTTS(text=text, lang='nl', slow=True)
+    tts.write_to_fp(mp3_fp)
+    return mp3_fp.getvalue()
+
+def pauze():
+    mp3_fp = io.BytesIO()
+    tts = gTTS("¤", lang='fr') 
+    tts.write_to_fp(mp3_fp)
+    return 15*mp3_fp.getvalue()
