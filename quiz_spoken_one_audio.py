64ac4830ac7a553609bbaab426961fb73a69d71b
DieterVerhofstadt <dieter.verhofstadt@gmail.com>
2025-07-07 14:59:29 +0200

    Update quiz_spoken_one_audio.py
    stop en test eleven

--- quiz_spoken_one_audio.py
+++ quiz_spoken_one_audio.py
@@ -62,3 +62,5 @@ with col2:
         create_one_mp3_quiz('csv/tourdefrance.csv')
         if st.button("Terug naar start"):
             go_to_home()
+if st.button("Stop")
+create_eleven_mp3('Leuk dat je deze quiz gespeeld hebt', voice="Tijs", model="eleven_monolingual_v1")
