9a9ee6368c400d5c1ddb694d5a1b8f4c11003969
DieterVerhofstadt <dieter.verhofstadt@gmail.com>
2025-07-07 15:58:48 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,30 +4,13 @@ from gtts import gTTS
 #set_api_key(st.secrets["elevenlabs"]["api_key"])
 
 def create_tts_mp3(text):
-    try:
-    	mp3_fp = io.BytesIO()
-    	tts = gTTS(text=text, lang='nl', slow=True)
-    	tts.write_to_fp(mp3_fp)
-    	return mp3_fp.getvalue()
-     except Exception as e:
-        st.error(f"Er ging iets mis: {e}")
-        return None
+    mp3_fp = io.BytesIO()
+    tts = gTTS(text=text, lang='nl', slow=True)
+    tts.write_to_fp(mp3_fp)
+    return mp3_fp.getvalue()
 def pauze(number):
     mp3_fp = io.BytesIO()
     tts = gTTS("¤", lang='fr') 
     tts.write_to_fp(mp3_fp)
     return number*mp3_fp.getvalue()
 
-# def create_eleven_mp3(text, voice, model):
-	try: 	
-    audio = generate(
-        text=text,
-        voice=voice,
-	model=model
-        stream=False  # stream=True geeft een generator, stream=False geeft bytes
-    )
-    mp3_fp = io.BytesIO(audio)  # audio is al mp3-bytestream
-    return mp3_fp
-  except Exception as e:
-        st.error(f"Er ging iets mis met ElevenLabs: {e}")
-        return None
