e8c662816b2834c446e60d9f7b9818ffdbc7963f
DieterVerhofstadt <dieter.verhofstadt@gmail.com>
2025-07-07 15:44:14 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import io
 from gtts import gTTS
+from elevenlabs import generate, play, save, set_api_key
+set_api_key(st.secrets["elevenlabs"]["api_key"])
 
 def create_tts_mp3(text):
     mp3_fp = io.BytesIO()
@@ -13,3 +15,16 @@ def pauze(number):
     tts.write_to_fp(mp3_fp)
     return number*mp3_fp.getvalue()
 
+def create_eleven_mp3(text, voice, model):
+    """
+    Genereert een mp3-bestand met TTS via ElevenLabs.
+    Geeft een BytesIO stream terug die je direct in Streamlit kan gebruiken.
+    """
+    audio = generate(
+        text=text,
+        voice=voice,
+	model=model
+        stream=False  # stream=True geeft een generator, stream=False geeft bytes
+    )
+    mp3_fp = io.BytesIO(audio)  # audio is al mp3-bytestream
+    return mp3_fp
