f007d4ad3f80fdce9d7ce7ea26ffc584b7a278ef
DieterVerhofstadt <dieter.verhofstadt@gmail.com>
2025-07-07 16:00:04 +0200

    Update quiz_spoken_one_audio.py

--- quiz_spoken_one_audio.py
+++ quiz_spoken_one_audio.py
@@ -1,7 +1,6 @@
 import csv
 import random
 from utils import create_tts_mp3
-#from utils import create_eleven_mp3
 from utils import pauze
 import streamlit as st
 import io
@@ -64,5 +63,4 @@ with col2:
         if st.button("Terug naar start"):
             go_to_home()
 
-#if st.button("Einde"):
-    #create_eleven_mp3('Leuk dat je deze quiz gespeeld hebt', voice="Tijs", model="eleven_monolingual_v1")
+
