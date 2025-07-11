572cb0eaa17cbdfc9a1eceea23f71e528c188e8a
DieterVerhofstadt <dieter.verhofstadt@gmail.com>
2025-07-11 14:39:18 +0200

    Update test_getting_wikipedia.py

--- test_getting_wikipedia.py
+++ test_getting_wikipedia.py
@@ -2,7 +2,7 @@ import streamlit as st
 from utils import get_pages_from_category
 categorie = "Winnaars_van_de_Nobelprijs_voor_Literatuur"
 pagina_titels = get_pages_from_category(categorie)
-if st.button("Go")
+if st.button("Go"):
 for titel in pagina_titels:
     print(titel)
     # Alle dataframes samenvoegen
