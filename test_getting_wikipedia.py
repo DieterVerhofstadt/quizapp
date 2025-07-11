d703fb07497a6c70a7977a25c7f68253851b2453
DieterVerhofstadt <dieter.verhofstadt@gmail.com>
2025-07-11 14:42:02 +0200

    Update test_getting_wikipedia.py

--- test_getting_wikipedia.py
+++ test_getting_wikipedia.py
@@ -5,8 +5,5 @@ pagina_titels = get_pages_from_category(categorie)
 if st.button("Go"):
     for titel in pagina_titels:
         print(titel)
-        # Alle dataframes samenvoegen
-        samengevoegd_df = pd.concat(dataframes, ignore_index=True)
-        # Resultaat opslaan naar een nieuwe CSV
-        samengevoegd_df.to_csv("csv/samengevoegd.csv", index=False)
+       
 
