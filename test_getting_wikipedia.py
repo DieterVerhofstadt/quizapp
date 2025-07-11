ae8b4f0d51ab29ed7a91a000adf34d4920dce4a7
DieterVerhofstadt <dieter.verhofstadt@gmail.com>
2025-07-11 14:44:27 +0200

    Update test_getting_wikipedia.py

--- test_getting_wikipedia.py
+++ test_getting_wikipedia.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from utils import get_pages_from_category
-categorie = "Winnaars_van_de_Nobelprijs_voor_Literatuur"
+categorie = "Winnaar_van_de_Nobelprijs_voor_Literatuur"
 pagina_titels = get_pages_from_category(categorie)
 if st.button("Go"):
     for titel in pagina_titels:
