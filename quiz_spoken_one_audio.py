60bf292e9fa7d803193f0c27e4c1b62d11c47221
DieterVerhofstadt <dieter.verhofstadt@gmail.com>
2025-11-08 12:55:13 +0100

    Update quiz_spoken_one_audio.py

--- quiz_spoken_one_audio.py
+++ quiz_spoken_one_audio.py
@@ -57,7 +57,10 @@ with col1:
         create_one_mp3_quiz('csv/aardrijkskunde.csv')
         if st.button("Terug naar start"):
             go_to_home()        
-
+    elif st.button("Schaduw Red Michiel"):
+        create_one_mp3_quiz('csv/Schaduw Red Michiel.csv')
+        if st.button("Terug naar start"):
+            go_to_home()      
 with col2:    
     if st.button("Wereldkampioenen F1"):
         create_one_mp3_quiz('csv/formule1.csv')
@@ -81,6 +84,7 @@ with col2:
         create_one_mp3_quiz('csv/samengevoegd.csv')
         if st.button("Terug naar start"):
             go_to_home()
+
 
 
 
