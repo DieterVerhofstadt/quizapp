77d855687062f59707ae24a6b28d30033a81ce39
DieterVerhofstadt <dieter.verhofstadt@gmail.com>
2025-11-09 16:54:11 +0100

    Update test_getting_wikipedia.py

--- test_getting_wikipedia.py
+++ test_getting_wikipedia.py
@@ -1,5 +1,29 @@
 import streamlit as st
-from utils import get_pages_from_category, get_summary
+
+def get_pages_from_category(category_name, limit=50):
+    S = requests.Session()
+    URL = "https://nl.wikipedia.org/w/api.php"
+    
+    params = {
+        "action": "query",
+        "list": "categorymembers",
+        "cmtitle": f"Categorie:{category_name}",
+        "cmlimit": limit,
+        "format": "json"
+    }
+    response = S.get(url=URL, params=params)
+    data = response.json()
+    return [item["title"] for item in data["query"]["categorymembers"]]
+
+def get_summary(title):
+    safe_title = title.replace(" ", "_")
+    url = f"https://nl.wikipedia.org/api/rest_v1/page/summary/{safe_title}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json().get("extract")
+    else:
+        return None
+
 categorie = "Winnaar_van_de_Nobelprijs_voor_Literatuur"
 pagina_titels = get_pages_from_category(categorie)
 if st.button("Go"):
