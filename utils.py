89d1eec0edb109aa6f27a8e0e38518a2cbe0715d
DieterVerhofstadt <dieter.verhofstadt@gmail.com>
2025-11-09 16:53:18 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -28,26 +28,4 @@ def merge_csv():
         df = pd.read_csv(volledig_pad)
         dataframes.append(df)
 
-def get_pages_from_category(category_name, limit=50):
-    S = requests.Session()
-    URL = "https://nl.wikipedia.org/w/api.php"
-    
-    params = {
-        "action": "query",
-        "list": "categorymembers",
-        "cmtitle": f"Categorie:{category_name}",
-        "cmlimit": limit,
-        "format": "json"
-    }
-    response = S.get(url=URL, params=params)
-    data = response.json()
-    return [item["title"] for item in data["query"]["categorymembers"]]
 
-def get_summary(title):
-    safe_title = title.replace(" ", "_")
-    url = f"https://nl.wikipedia.org/api/rest_v1/page/summary/{safe_title}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json().get("extract")
-    else:
-        return None
