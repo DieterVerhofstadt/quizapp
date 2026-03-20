0553d11b28528a6deafb6edda9672498336b6da1
DieterVerhofstadt <dieter.verhofstadt@gmail.com>
2026-03-20 23:13:35 +0100

    Update quiz_spoken_one_audio.py

--- quiz_spoken_one_audio.py
+++ quiz_spoken_one_audio.py
@@ -75,7 +75,7 @@ with col1:
         create_one_mp3_quiz('csv/gastronomie.csv')
         if st.button("Terug naar start"):
             go_to_home()
-    elif st.button("Varia waaronder fauna en flora"):
+    elif st.button("Varia"):
         create_one_mp3_quiz('csv/varia.csv')
         if st.button("Terug naar start"):
             go_to_home()
